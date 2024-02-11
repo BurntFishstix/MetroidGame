bb390eb81d00ea2195d4671c878cd960dd3db3a7
Brian Rosario <149282530+sTakumi12@users.noreply.github.com>
2024-02-10 23:23:26 -0500

    lives

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 3;
     public int health;
+    public TextMeshProUGUI livesText;
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        livesText.text = "Lives: " + health;
     }
 
     // Update is called once per frame
@@ -21,6 +25,7 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+        livesText.text = "Lives: " + health;
         if(health <= 0)
         {
             Destroy(gameObject);
